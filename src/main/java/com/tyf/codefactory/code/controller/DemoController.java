069a41837c25a97e4598f524e499af747d354e9d
tyf <63250124@qq.com>
2019-02-27 17:47:15 +0800

    .......

--- src/main/java/com/tyf/codefactory/code/controller/DemoController.java
+++ src/main/java/com/tyf/codefactory/code/controller/DemoController.java
@@ -1,10 +1,10 @@
 package com.tyf.codefactory.code.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/demo")
 public class DemoController {
 
