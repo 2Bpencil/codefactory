4b894f413f0b2299cf576c88c4c29b21fe5363ed
tyf <63250124@qq.com>
2019-07-05 16:17:19 +0800

    .......

--- src/main/java/com/tyf/codefactory/code/controller/DemoController.java
+++ src/main/java/com/tyf/codefactory/code/controller/DemoController.java
@@ -1,6 +1,11 @@
 package com.tyf.codefactory.code.controller;
 
+import com.tyf.codefactory.code.entity.User;
+import com.tyf.codefactory.code.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,8 +13,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/demo")
 public class DemoController {
 
+    @Autowired
+    private UserServiceImpl userService;
+
     @RequestMapping(value = "hello",method = RequestMethod.GET)
-    public String hello(){
+    public String hello(Model model){
+        Page<User> userPages = userService.findPage(1);
+        model.addAttribute("userList",userPages);
+        System.out.println("bbbaaa:"+userPages.getTotalElements());
         return "hello";
     }
 
